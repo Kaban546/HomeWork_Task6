7c7d4457716ff6f14a9dddf9f07e5c979eb9cbbf
Maxim Saychuk <maks.saychuk@gmail.com>
2023-09-25 15:19:59 +0300

    frathure_add_added

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -20,6 +20,27 @@ namespace Task6_git
             Console.WriteLine("Ведите функцию +");
             string operand = Console.ReadLine();
             int result = 0;
+            switch (operand)
+            {
+                case "+":
+                    {
+                        result = number1 + number2;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Введена неизвестная функция пожалуйста введите ещё раз");
+                        goto RetryOperand;
+                    }
+
+            }
+            Console.WriteLine($"Результат между {number1}{operand}{number2}={result}");
+            Console.WriteLine("Попробовать ещё раз?\ny/n");
+            string rest = Console.ReadLine();
+            if (rest.ToLower() == "y")
+            {
+                goto Retry;
+            }
         }
     }
 }
