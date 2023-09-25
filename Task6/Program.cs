eff15c0d4c9baf9f088faa59a6c77342e0946e1d
Maxim Saychuk <maks.saychuk@gmail.com>
2023-09-25 15:14:19 +0300

    Basic user imput

---
+++ Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Retry:
+            Console.WriteLine("Введите чисо 1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            int number2 = int.Parse(Console.ReadLine());
+        RetryOperand:
+            Console.WriteLine("Ведите функцию +");
+            string operand = Console.ReadLine();
+            int result = 0;
+        }
+    }
+}
+
